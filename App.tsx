da5e79abde832cbe5e88240861352a6c343971ea
Rodrigobr2003 <ronaldobr2003@gmail.com>
2024-11-21 17:01:50 -0300

    :wrench: feat: Ajusta configurações tela index

--- App.tsx
+++ App.tsx
@@ -1,20 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import {
+  useFonts,
+  PressStart2P_400Regular,
+} from "@expo-google-fonts/press-start-2p";
 
 export default function App() {
+  const [fonteCarregada] = useFonts({
+    PressStart2P_400Regular,
+  });
+
+  if (!fonteCarregada) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View>
+        <Text style={styles.titulo}>Conectando</Text>
+        <Text style={styles.titulo}>o mundo em</Text>
+        <Text style={styles.titulo}>uma esfera de</Text>
+        <Text style={styles.titulo}>possibilidades</Text>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#1D1D1D",
+    alignItems: "center",
+    paddingTop: 60,
+  },
+
+  titulo: {
+    color: "#fff",
+    fontSize: 26,
+    textAlign: "center",
+    fontFamily: "PressStart2P_400Regular",
   },
 });
