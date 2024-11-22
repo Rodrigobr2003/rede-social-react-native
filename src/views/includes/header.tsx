40418fbbdd0608ae71391700e8cba23fa0c73e7e
Rodrigobr2003 <ronaldobr2003@gmail.com>
2024-11-22 15:34:39 -0300

    :lipstick: feat: Cria layout header

---
+++ src/views/includes/header.tsx
@@ -0,0 +1,46 @@
+import {
+  Text,
+  View,
+  StyleSheet,
+  TextInput,
+  TouchableHighlight,
+} from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+
+export default function Header() {
+  return (
+    <View style={styles.container}>
+      <TextInput placeholder="Testando" style={styles.input}></TextInput>
+
+      <TouchableHighlight style={styles.burguerButton}>
+        <Text>
+          <Ionicons name="menu-outline" size={40} color={"#2B378F"} />
+        </Text>
+      </TouchableHighlight>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#fff",
+    alignItems: "center",
+    flexDirection: "row",
+    width: "100%",
+  },
+
+  input: {
+    width: 300,
+    backgroundColor: "#F0F2F5",
+    marginVertical: 20,
+    marginHorizontal: 30,
+    padding: 10,
+    borderRadius: 18,
+    color: "#000",
+    fontSize: 20,
+  },
+
+  burguerButton: {
+    marginVertical: 20,
+  },
+});
