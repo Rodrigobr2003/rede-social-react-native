093f525e0b59e8aa6ba3138345813f803d7ead20
Rodrigobr2003 <ronaldobr2003@gmail.com>
2024-12-16 17:34:01 -0300

    :lipstick: feat: Adiciona estrutura tela pesquisa

--- src/app/telaPesquisa.tsx
+++ src/app/telaPesquisa.tsx
@@ -1,13 +1,40 @@
+import { Ionicons } from "@expo/vector-icons";
 import { ScrollView, StyleSheet, View, Text } from "react-native";
 
 export default function TelaPesquisa() {
   return (
-    <ScrollView>
-      <View>
-        <Text>AAAA</Text>
-      </View>
+    <ScrollView style={{ backgroundColor: "#fff", borderTopWidth: 1 }}>
+      {/* <View style={styles.profileSec}>
+        <Ionicons
+          name="person"
+          size={45}
+          style={{ width: "20%", paddingHorizontal: 10 }}
+        ></Ionicons>
+
+        <Text style={styles.nomeUser}>Nome do Usuário</Text>
+
+        <Ionicons
+          name="ellipsis-horizontal"
+          size={30}
+          style={{ width: "20%", paddingHorizontal: 20 }}
+        ></Ionicons>
+      </View> */}
     </ScrollView>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  profileSec: {
+    width: "90%",
+    marginHorizontal: "auto",
+    marginVertical: 15,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+
+  nomeUser: {
+    fontSize: 20,
+    fontWeight: "bold",
+    width: "60%",
+  },
+});
