5ba38312b29c56bfdf3c4048d632f093c329aacf
Rodrigobr2003 <ronaldobr2003@gmail.com>
2024-12-16 20:20:49 -0300

    :lipstick: feat: Adiciona menu navegação

--- src/app/includes/header.tsx
+++ src/app/includes/header.tsx
@@ -1,55 +1,102 @@
 import { Text, View, StyleSheet, TextInput, Pressable } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import { router, useSegments } from "expo-router";
+import { useState } from "react";
 
 export default function Header({ display }: { display: any }) {
   function navigate() {
     router.navigate("/telaPesquisa");
   }
 
+  const [visibleNav, setVisibleNav] = useState(false);
+
   const segments = useSegments();
   const currentPath = segments.join("/");
 
   return (
-    <View style={[styles.container, display]}>
-      <Pressable
-        style={[
-          styles.backButton,
-          currentPath == "home" ? { display: "none" } : { display: "flex" },
-        ]}
-        onPress={() => {
-          router.back();
-        }}
-      >
-        <Text>
-          <Ionicons name="chevron-back" size={40} color={"#2B378F"} />
-        </Text>
-      </Pressable>
+    <>
+      <View style={[styles.container, display]}>
+        <Pressable
+          style={[
+            styles.backButton,
+            currentPath == "home" ? { display: "none" } : { display: "flex" },
+          ]}
+          onPress={() => {
+            router.back();
+          }}
+        >
+          <Text>
+            <Ionicons name="chevron-back" size={40} color={"#2B378F"} />
+          </Text>
+        </Pressable>
 
-      <TextInput
-        placeholder="Pesquise no Orbee..."
-        style={[
-          styles.input,
-          currentPath == "home"
-            ? { width: 300 }
-            : { width: 350, marginLeft: "auto" },
-        ]}
-        onPress={() => {
-          navigate();
-        }}
-      ></TextInput>
+        <TextInput
+          placeholder="Pesquise no Orbee..."
+          style={[
+            styles.input,
+            currentPath == "home"
+              ? { width: 300 }
+              : { width: 350, marginLeft: "auto" },
+          ]}
+          onPress={() => {
+            navigate();
+          }}
+        ></TextInput>
 
-      <Pressable
+        <Pressable
+          style={[
+            styles.burguerButton,
+            currentPath == "home" ? { display: "flex" } : { display: "none" },
+            visibleNav ? { display: "none" } : { display: "flex" },
+          ]}
+          onPress={() => {
+            setVisibleNav(true);
+          }}
+        >
+          <Text>
+            <Ionicons name="menu-outline" size={40} color={"#2B378F"} />
+          </Text>
+        </Pressable>
+
+        <Pressable
+          style={[
+            styles.burguerButton,
+            currentPath == "home" ? { display: "flex" } : { display: "none" },
+            visibleNav ? { display: "flex" } : { display: "none" },
+          ]}
+          onPress={() => {
+            setVisibleNav(false);
+          }}
+        >
+          <Text>
+            <Ionicons name="close-outline" size={40} color={"#2B378F"} />
+          </Text>
+        </Pressable>
+      </View>
+
+      <View
         style={[
-          styles.burguerButton,
-          currentPath == "home" ? { display: "flex" } : { display: "none" },
+          visibleNav ? { display: "flex" } : { display: "none" },
+          styles.nav,
         ]}
       >
-        <Text>
-          <Ionicons name="menu-outline" size={40} color={"#2B378F"} />
-        </Text>
-      </Pressable>
-    </View>
+        <Pressable>
+          <Text style={styles.navItem}>Home</Text>
+        </Pressable>
+
+        <Pressable>
+          <Text style={styles.navItem}>Amigos</Text>
+        </Pressable>
+
+        <Pressable>
+          <Text style={styles.navItem}>Notificações</Text>
+        </Pressable>
+
+        <Pressable>
+          <Text style={styles.navItem}>Perfil</Text>
+        </Pressable>
+      </View>
+    </>
   );
 }
 
@@ -79,5 +126,20 @@ const styles = StyleSheet.create({
   backButton: {
     marginVertical: 20,
     marginHorizontal: 10,
+  },
+
+  nav: {
+    position: "absolute",
+    backgroundColor: "#fff",
+    marginTop: 80,
+    zIndex: 1,
+    width: "100%",
+  },
+
+  navItem: {
+    textAlign: "center",
+    fontSize: 40,
+    marginVertical: 5,
+    fontWeight: "700",
   },
 });
