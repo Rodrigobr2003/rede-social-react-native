ad63898b8648b00142817fae0fecf3aaf65931ab
Rodrigobr2003 <ronaldobr2003@gmail.com>
2024-12-20 11:23:22 -0300

    :wrench: feat: Adiciona UserProvider

--- frontend/src/app/_layout.tsx
+++ frontend/src/app/_layout.tsx
@@ -1,5 +1,6 @@
 import { StyleSheet } from "react-native";
 import { useEffect, useState } from "react";
+import { UserProvider } from "./includes/UserProvider";
 
 import { Stack, useSegments } from "expo-router";
 
@@ -8,36 +9,12 @@ import Header from "./includes/header";
 export default function Layout() {
   const segments = useSegments();
   const [headerVisible, setHeaderStyle] = useState(false);
-  const [user, setUSer] = useState(null);
 
   useEffect(() => {
     const currentPath = segments.join("/");
 
     if (currentPath !== "") {
       setHeaderStyle(true);
-
-      const fetchUserData = async () => {
-        try {
-          const response = await fetch("http://10.0.2.2:3008/getUserData", {
-            method: "GET",
-            mode: "cors",
-            headers: {
-              "content-type": "application/json",
-            },
-          });
-
-          if (!response.ok) {
-            throw new Error(`Erro na resposta do servidor: ${response.status}`);
-          }
-
-          const data = await response.json();
-          setUSer(data);
-        } catch (error) {
-          console.log("Erro ao buscar dados no fetch: ", error);
-        }
-      };
-
-      fetchUserData();
     } else {
       setHeaderStyle(false);
     }
@@ -49,47 +26,49 @@ export default function Layout() {
         display={headerVisible ? styles.visibleHeader : styles.hiddenHeader}
       ></Header>
 
-      <Stack>
-        <Stack.Screen
-          name="index"
-          options={{ headerShown: false }}
-        ></Stack.Screen>
+      <UserProvider>
+        <Stack>
+          <Stack.Screen
+            name="index"
+            options={{ headerShown: false }}
+          ></Stack.Screen>
 
-        <Stack.Screen
-          name="home"
-          options={{ headerShown: false }}
-        ></Stack.Screen>
+          <Stack.Screen
+            name="home"
+            options={{ headerShown: false }}
+          ></Stack.Screen>
 
-        <Stack.Screen
-          name="telaPesquisa"
-          options={{ headerShown: false }}
-        ></Stack.Screen>
+          <Stack.Screen
+            name="telaPesquisa"
+            options={{ headerShown: false }}
+          ></Stack.Screen>
 
-        <Stack.Screen
-          name="amigos"
-          options={{ headerShown: false }}
-        ></Stack.Screen>
+          <Stack.Screen
+            name="amigos"
+            options={{ headerShown: false }}
+          ></Stack.Screen>
 
-        <Stack.Screen
-          name="notificacoes"
-          options={{ headerShown: false }}
-        ></Stack.Screen>
+          <Stack.Screen
+            name="notificacoes"
+            options={{ headerShown: false }}
+          ></Stack.Screen>
 
-        <Stack.Screen
-          name="perfil"
-          options={{ headerShown: false }}
-        ></Stack.Screen>
+          <Stack.Screen
+            name="perfil"
+            options={{ headerShown: false }}
+          ></Stack.Screen>
 
-        <Stack.Screen
-          name="perfilProcurado"
-          options={{ headerShown: false }}
-        ></Stack.Screen>
+          <Stack.Screen
+            name="perfilProcurado"
+            options={{ headerShown: false }}
+          ></Stack.Screen>
 
-        <Stack.Screen
-          name="chatProfile"
-          options={{ headerShown: false }}
-        ></Stack.Screen>
-      </Stack>
+          <Stack.Screen
+            name="chatProfile"
+            options={{ headerShown: false }}
+          ></Stack.Screen>
+        </Stack>
+      </UserProvider>
     </>
   );
 }
---
+++ frontend/src/app/includes/UserProvider.tsx
@@ -0,0 +1,57 @@
+import { createContext, ReactNode, useEffect, useState } from "react";
+
+interface User {
+  nome: string;
+  sobrenome: string;
+  email: string;
+  genero: string;
+  descicao: string;
+  amigos: Object[];
+  notificacoes: Object[];
+}
+
+interface UserContextType {
+  user: User | null;
+  setUser: React.Dispatch<React.SetStateAction<User | null>>;
+}
+
+export const UserContext = createContext<UserContextType | null>(null);
+
+interface UserProviderProps {
+  children: ReactNode;
+}
+
+export function UserProvider({ children }: UserProviderProps) {
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const response = await fetch("http://10.0.2.2:3008/getUserData", {
+          method: "GET",
+          mode: "cors",
+          headers: {
+            "content-type": "application/json",
+          },
+        });
+
+        if (!response.ok) {
+          throw new Error(`Erro na resposta do servidor: ${response.status}`);
+        }
+
+        const data = await response.json();
+        setUser(data);
+      } catch (error) {
+        console.log("Erro ao buscar dados no fetch: ", error);
+      }
+    };
+
+    fetchUserData();
+  }, []);
+
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+}
