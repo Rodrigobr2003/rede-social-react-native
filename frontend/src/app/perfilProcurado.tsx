269d5f0d0086bddb5ba10c46c4fd03e35a6887e4
Rodrigobr2003 <ronaldobr2003@gmail.com>
2025-01-07 12:12:51 -0300

    :lipstick: feat: Ajusta erro exibição

--- frontend/src/app/perfilProcurado.tsx
+++ frontend/src/app/perfilProcurado.tsx
@@ -85,6 +85,8 @@ export default function PerfilProcurado() {
                 />
               </Pressable>
             );
+          } else {
+            return null;
           }
         })}
       </>
